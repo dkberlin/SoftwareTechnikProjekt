bbd8857c2ee1049cae6e80020705557d960835f7
DK@Home <decay@mail.de>
2019-05-11 13:16:07 +0200

    moving modules now with events for decoupling

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -16,20 +16,60 @@ using System.Diagnostics;
 
 namespace SoftwareTechnikProjekt
 {
-    /// <summary>
-    /// Interaktionslogik für MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        //private static MainWindow _instance;
+        public static MainWindow AppWindow { get; private set; }
+        private static readonly object _padLock = new object();
+
+        public event ModuleMoved OnModuleMoved;
+        public delegate void ModuleMoved(object selectedModule, ListBox FromList, ListBox ToList);
+
+        //public static MainWindow Instance
+        //{
+        //    get
+        //    {
+        //        lock (_padLock)
+        //        {
+        //            if (_instance == null)
+        //            {
+        //                _instance = new MainWindow();
+        //            }
+        //            return _instance;
+        //        }
+        //    }
+        //}
+
         public MainWindow()
         {
+            AppWindow = this;
             InitializeComponent();
             SetupOnStartApplication();
         }
 
         private void SetupOnStartApplication()
         {
-            ModuleController.GetAllModules();
+            //TODO: check for save
+            ModuleController.Instance.SetupEvents();
+            var modules = ModuleController.Instance.GetAllModules();
+            foreach(var module in modules)
+            {
+                openModules.Items.Add(module.Title);
+            }
+        }
+
+        private void AddToPlannedButton_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = openModules.SelectedItem;
+            //ModuleController.MoveToList(selectedItem, openModules, plannedModules);
+            OnModuleMoved?.Invoke(selectedItem, openModules, plannedModules);
+        }
+
+        private void RemoveFromPlanned_Click(object sender, RoutedEventArgs e)
+        {
+            var selectedItem = plannedModules.SelectedItem;
+            //ModuleController.Instance.MoveToList(selectedItem, plannedModules, openModules);
+            OnModuleMoved?.Invoke(selectedItem, plannedModules, openModules);
         }
     }
 }
--- ModuleController.cs
+++ ModuleController.cs
@@ -1,13 +1,37 @@
 ﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Reflection;
+using System.Windows.Controls;
 
 namespace SoftwareTechnikProjekt
 {
     internal class ModuleController
     {
-        internal static void GetAllModules()
+        private static ModuleController _instance;
+        private static readonly object _padLock = new object();
+
+        public ModuleController()
+        {
+        }
+
+        public static ModuleController Instance
+        {
+            get
+            {
+                lock (_padLock)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new ModuleController();
+                    }
+                    return _instance;
+                }
+            }
+        }
+
+        internal List<CollegeModule> GetAllModules()
         {
             var assembly = Assembly.GetExecutingAssembly();
             var resourceName = "SoftwareTechnikProjekt.modules.json";
@@ -16,8 +40,25 @@ namespace SoftwareTechnikProjekt
                 using (StreamReader reader = new StreamReader(stream))
             {
                 string result = reader.ReadToEnd();
-                var modules = JsonConvert.DeserializeObject<List<CollegeModule>>(result);
+                return JsonConvert.DeserializeObject<List<CollegeModule>>(result);
             }
+        }
+
+        internal void MoveToList(object selectedItem, ListBox originalBox, ListBox newBox)
+        {
+            newBox.Items.Add(selectedItem);
+            originalBox.Items.Remove(selectedItem);
+        }
+
+        internal void SetupEvents()
+        {
+            MainWindow.AppWindow.OnModuleMoved += OnModuleHasBeenMoved;
+        }
+
+        private void OnModuleHasBeenMoved(object selectedModule, ListBox FromList, ListBox ToList)
+        {
+            ToList.Items.Add(selectedModule);
+            FromList.Items.Remove(selectedModule);
         }
     }
 }
